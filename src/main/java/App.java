9130eee30eaa200719e9267e6051db6baf308457
Chrono-90 <chrono-90@bk.ru>
2021-11-23 16:00:04 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,23 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+
+
+
+        System.out.println(bean==bean1);
+        System.out.println(cat==cat2);
     }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,14 +1,15 @@
-public class HelloWorld {
- 
-    private String message;
- 
-    public String getMessage() {
+public class Cat {
+    public Cat() {
+    }
 
+    private String message;
+
+    public String getMessage() {
         return message;
     }
- 
+
     public void setMessage(String message) {
         this.message = message;
     }
-     
+
 }
